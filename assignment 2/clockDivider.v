340af4fd6aa8f866bfa886cd607499d81622e006
PhatHNguyen <66038812+PhatHNguyen@users.noreply.github.com>
2024-07-05 11:16:51 -0600

    fix

--- assignment 2/clockDivider.v
+++ assignment 2/clockDivider.v
@@ -1,17 +1,16 @@
 module ClockDivider (
-
-	input clk 
+	input 	   clk, 
 	output reg slower_clk	
 	);
 	
-	wire [31:0] counter = 0;
+	reg [31:0] counter = 0;
 	
 always@(posedge clk) begin 
 	if(counter >= 12_000_000) begin 
 		slower_clk <= ~slower_clk;
 		counter <= 0;
 	end else begin 
-		coutner <= counter + 1; 
+		counter <= counter + 1; 
 	end
 end 
 
