571c71afefeccd20ac66445ef7c4db7a213cb894
PhatHNguyen <66038812+PhatHNguyen@users.noreply.github.com>
2024-07-12 14:21:47 -0600

    add headers

--- assignment 1/clockDivider.v
+++ assignment 1/clockDivider.v
@@ -1,3 +1,7 @@
+/*
+The clockDivider module inputs a fast clock and outputs 25 MHZ clk by
+using a counter that will invert the input.
+*/
 module ClockDivider (
 	input 	   clk, 
 	output reg slower_clk	
@@ -5,7 +9,7 @@ module ClockDivider (
 	
 	reg [31:0] counter = 0;
 	
-always@(posedge clk) begin 
+always@(posedge clk) begin  
 	if(counter >= 12_000_000) begin 
 		slower_clk <= ~slower_clk;
 		counter <= 0;
