a40c32b924b10b28a6e1f9af92d9dafea8777290
PhatHNguyen <66038812+PhatHNguyen@users.noreply.github.com>
2024-08-27 23:40:05 -0600

    Update clockDivider.v

--- assignment 1/clockDivider.v
+++ assignment 1/clockDivider.v
@@ -2,20 +2,31 @@
 The clockDivider module inputs a fast clock and outputs 25 MHZ clk by
 using a counter that will invert the input.
 */
-module ClockDivider (
+module Clock_div(
 	input 	   clk, 
+	input reset,
 	output reg slower_clk	
 	);
 	
 	reg [31:0] counter = 0;
 	
-always@(posedge clk) begin  
-	if(counter >= 12_000_000) begin 
+always@(posedge clk) begin
+	if(reset) begin
+		slower_clk <= 0;
+		counter <= 0;
+	end else if(counter < 32'd1200000) begin 
+		counter <= counter + 1; 
+	end else begin 
 		slower_clk <= ~slower_clk;
 		counter <= 0;
-	end else begin 
-		counter <= counter + 1; 
 	end
 end 
+
+always@(posedge clk) begin
+	if(slower_clk) begin
+		slower_clk <= ~slower_clk;
+		counter <= 0;
+	end
+end
 
 endmodule 
