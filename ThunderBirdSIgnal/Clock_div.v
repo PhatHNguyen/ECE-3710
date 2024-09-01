ab3c0f326a7f24a7c276a16b9a302330ea8b0699
PhatHNguyen <66038812+PhatHNguyen@users.noreply.github.com>
2024-08-31 18:33:13 -0600

    Update Clock_div.v

--- ThunderBirdSIgnal/Clock_div.v
+++ ThunderBirdSIgnal/Clock_div.v
@@ -1,5 +1,5 @@
 /*
-The clockDivider module inputs a fast clock and outputs 25 MHZ clk by
+The clockDivider module inputs a fast clock and outputs an enable signal by
 using a counter that will invert the input.
 */
 module Clock_div(
@@ -9,7 +9,11 @@ module Clock_div(
 	);
 	
 	reg [31:0] counter = 0;
-	
+
+/* reset - set the counter and output to 0
+   slower_clk - if slower_clk is high, set it to low next cycle so that it creates a blimp
+   counter - if reaches to 20 million then invert the output "clk" (enable)
+*/
 always@(posedge clk) begin
 	if(!reset) begin
 		slower_clk <= 0;
@@ -17,7 +21,7 @@ always@(posedge clk) begin
 	end else if(slower_clk) begin 
 		slower_clk <= 0;
 		counter <= 0;
-	end else if(counter < 32'd100)begin
+	end else if(counter < 32'd20000000)begin
 		counter <= counter + 1; 
 	end else begin 
 		slower_clk <= ~slower_clk;
