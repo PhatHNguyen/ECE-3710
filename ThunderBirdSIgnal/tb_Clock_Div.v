59a3af1734048d5e493820a721bf74de52a9a5fc
PhatHNguyen <66038812+PhatHNguyen@users.noreply.github.com>
2024-08-31 18:38:26 -0600

    Update tb_Clock_Div.v

--- ThunderBirdSIgnal/tb_Clock_Div.v
+++ ThunderBirdSIgnal/tb_Clock_Div.v
@@ -1,16 +1,28 @@
+/*
+* File Name: tb_Clock_Div.v
+* Author:  Phat Nguyen
+* Create Date:    8/20/2024
+* Purpose: test ThunderBiedSignal.v for correctness
+* ECE 3710
+*/
 module tb_Clock_Div();
 
 reg clk;
 reg reset;
 wire slower_clk;
 
+// instansitate module 
 Clock_div UUT(.clk(clk),.reset(reset),.slower_clk(slower_clk));
 
+// Toggle clk every 5 ns 
 initial begin
 	clk = 0;
 	forever #5 clk = ~clk;
 end
 
+// test if the output, slower_clk, go high for only one cycle
+// Note: since the counter is 20 million, you will need to set the simulation 
+// to 100 ms 
 initial begin
 	reset = 1;
 	repeat (10) @(posedge clk);
