d0bc27f6813f2db7cc56a1c6a5d51d9f77b9a666
PhatHNguyen <66038812+PhatHNguyen@users.noreply.github.com>
2024-09-05 16:16:00 -0600

    Update tb_ThunderBirdSignal.v

--- ThunderBirdSIgnal/tb_ThunderBirdSignal.v
+++ ThunderBirdSIgnal/tb_ThunderBirdSignal.v
@@ -39,13 +39,15 @@ end
 
 // simulate left turn signal (checking for complete sequence)
 task turnleft;
-   begin   
+   begin  
+    // reset the fsm 
     reset = 0;
     left = 1;
     right = 1;
     hazard = 1;
     enable = 0;
     repeat(5) @(posedge clk);
+    // press left 
     enable = 1;
     left = 0;
     right = 1;
@@ -59,11 +61,13 @@ endtask
 // simulate right turn signal (checking for complete sequence)
 task turnright;
  begin 
+// press right
 left = 1;
 right = 0;
 reset = 1;
 hazard = 1;
  repeat(8) @(posedge clk);
+// reset fsm 
 reset = 0;
 end
 endtask
@@ -71,30 +75,37 @@ endtask
 // simulate hazard signal (checking for alternation between [000000] and [111111])
 task harzardcheck;
 begin
+// press hazard 
 left = 1;
 right = 1;
 reset = 1;
 hazard = 0;
  repeat(5) @(posedge clk);
 reset = 0;
+// reset fsm 
 end
 endtask
 
 // simulate hazard assertion during a left/right turn signal sequence 
 task turnsignal_interuption;
  begin  
+// presss left 
 left = 0;
 right = 1;
 reset = 1;
 hazard = 1;
  repeat(4) @(posedge clk);
+// press right
 right = 0;
  repeat(2) @(posedge clk);
+// presss left 
 left = 0;
  repeat(2) @(posedge clk);
+//press hazard
 hazard = 0;
  repeat(10) @(posedge clk);
 reset = 0;
+// reset fsm 
 end
 endtask 
 
