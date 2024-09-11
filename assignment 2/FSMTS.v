3f9a59faec7fcfbde7a06a6dd3cae4b75c2b5552
PhatHNguyen <66038812+PhatHNguyen@users.noreply.github.com>
2024-09-11 10:02:33 -0600

    Update FSMTS.v

--- assignment 2/FSMTS.v
+++ assignment 2/FSMTS.v
@@ -1,121 +1,139 @@
-module FSMTS ( 
-	input [3:0] buttons,
-		        clk,
-   output reg [5:0] LED_status
- );
- 
-parameter left   = 4'b0111;
-parameter right  = 4'b1011;
-parameter reset  = 4'b1101;
-parameter hazard = 4'b1110;
+/* File Name:  ThunderBirdSignal.v
+* Author:  Phat Nguyen
+* Create Date:    8/20/2024
+* Purpose: Allow user to signal left,right, and hazard sign
+* ECE 3710 ThunderBird Signal
+*/
+module ThunderBirdSignal (
+    input left, 
+    right,
+    reset,
+    hazard,
+    clk,
+    enable,
+    output reg [5:0] status
+);
 
-parameter Neutral = 6'b000000;
-parameter L1      = 6'b001000; 
-parameter L2      = 6'b011000; 
-parameter L3      = 6'b111000; 
-parameter R1      = 6'b000100; 
-parameter R2      = 6'b000110; 
-parameter R3      = 6'b000111; 
-parameter Hazard  = 6'b111111; 
+// stages for FSM
+parameter Neutral = 6'b000000; // No lights 
+parameter L1 = 6'b001000;  // left turn signal stage 1
+parameter L2 = 6'b011000;  // left turn signal stage 2
+parameter L3 = 6'b111000;  // left turn signal stage 3
+parameter R1 = 6'b000100;  // right turn signal stage 1
+parameter R2 = 6'b000110;  // right turn signal stage 2
+parameter R3 = 6'b000111;  // right turn signal stage 3
+parameter Hazard = 6'b111111; // Hazard singal 
 
-reg [5:0] current_state;
-reg [5:0] next_state;
+reg [5:0] current_state = Neutral;
+reg [5:0] next_state = Neutral;
 
-
-// Next state prediction 
-always @* begin
-    case(buttons)
-	 
-    reset: begin
-        next_state <=  Neutral;
-    end 
-	 
-	 hazard: begin 
-		 next_state <= (current_state == Neutral) ? Hazard : Neutral;
-	 end
-	 
-	 left: begin 
-	   if(current_state == L1)
-			next_state <= L2;
-		else if(current_state == L2)
-			next_state <= L3;
-		else if(current_state == L3)
-			next_state <= Neutral;
-		else 
-			next_state <= L1;
-	 end
-	 
-	 right: begin 
-	   if(current_state == R1)
-			next_state <= R2;
-		else if(current_state == R2)
-			next_state <= R3;
-		else if(current_state == R3)
-			next_state <= Neutral;
-		else 
-			next_state <= R1;
-	 end
-  
-	default: begin
-		if(current_state == L1)
-			next_state <= L2;
-		else if(current_state == L2)
-			next_state <= L3;
-		else if(current_state == L3)
-			next_state <= Neutral;
-		else if(current_state == R1)
-			next_state <= R2;
-		else if(current_state == R2)
-			next_state <= R3;
-		else if(current_state == R3)
-			next_state <= Neutral;
-		else 
-			next_state <= Neutral;
-	end
-endcase 
-end
-
-//state progression process
+// update current_state 
 always @(posedge clk) begin
-    if(buttons == reset) begin
+    if(~reset) begin
         current_state <= Neutral;
-		  next_state <= Neutral;
-    end else if(buttons == hazard) begin
-        current_state <= Hazard;
-    end else begin 
+    end else if(enable) begin 
         current_state <= next_state;
     end
 end 
 
-//Output Driving Process
-always@(current_state) begin 
-	case(current_state) 
-	
-	Neutral: begin
-		LED_status <= 6'b000000;
-	end 
-	L1: begin
-		LED_status <= 6'b001000;
-	end 
-	L2: begin
-		LED_status <= 6'b011000;
-	end 
-	L3: begin
-		LED_status <= 6'b111000;
-	end 
-	R1: begin
-		LED_status <= 6'b000100;
-	end 
-	R2: begin
-		LED_status <= 6'b000110;
-	end 
-	R3: begin
-		LED_status <= 6'b000111;
-	end 
-	Hazard: begin
-		LED_status <= 6'b111111;
-	end 
-	endcase 
+// FSM: update the next_state and set the status LED to a specific result depending on the current_state
+// Note: is hazard has been pressed, go to the hazard state next clk cycle
+always @(*) begin
+    case(current_state)
+
+     // NO LEDS is active, waits for an input 
+     Neutral: begin
+	// presss any of the inputs will go to a specific stage
+	if (!hazard)
+	   next_state <= Hazard;
+	else if (!left) 
+            next_state <= L1;
+        else if (!right)
+            next_state <= R1;
+        else
+            next_state <= Neutral;
+        status <=  6'b000000;
+    end 
+
+    // Activate the first left signal (first left light is on)     
+    L1: begin
+	// presss hazard, go to hazard stage else continue the left stage
+	if (~hazard) 
+            next_state <= Hazard;
+        else
+            next_state <= L2;
+        status <=  6'b001000;
+    end
+
+    // Activate the second left signal (first and second left light is on)   
+    L2: begin 
+	// presss hazard, go to hazard stage else continue the left stage
+	if (!hazard) 
+            next_state <= Hazard;
+        else
+            next_state <= L3;
+        status <=  6'b011000;
+    end
+
+    // Activate the last left signal  (all 3 left signal )      
+    L3: begin 
+	// presss any of the inputs will go to a specific stage
+	if (!hazard)
+	    next_state <= Hazard;	
+	else if (!left) 
+            next_state <= L1;
+        else if (!right)
+            next_state <= R1;
+        else
+            next_state <= Neutral;
+        status <=  6'b111000;
+    end
+
+    // Activate the first right signal (first right light is on)    
+    R1: begin 
+	// presss hazard, go to hazard stage else continue the right stage
+	if (!hazard) 
+            next_state <= Hazard;
+        else
+            next_state <= R2;
+        status <=  6'b000100;
+    end
+
+    // Activate the second right signal (first and second right light is on)     
+    R2: begin 
+	// presss hazard, go to hazard stage else continue the right stage
+	if (!hazard) 
+            next_state <= Hazard;
+        else
+            next_state <= R3;
+        status <=  6'b000110;
+    end
+
+    // Activate the last right signal (all 3 right signal )       
+    R3: begin 
+	// presss any of the inputs will go to a specific stage
+	if (!hazard)
+	    next_state <= Hazard;
+        else if (!right) 
+            next_state <= R1;
+        else if (!left)
+            next_state <= L1;
+        else
+            next_state <= Neutral;
+        status <=  6'b000111;
+    end
+
+    // Activate all LEDS     
+    Hazard: begin
+	next_state <= Neutral;
+        status <=  6'b111111;
+    end
+   // should never occur
+  default: begin
+            next_state = Neutral; 
+            status = 6'b000000;
+        end
+endcase 
 end
 
 endmodule
