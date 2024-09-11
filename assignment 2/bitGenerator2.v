1eb0a22e9c6135f6365b9e7ca30dd13b04d8a3d8
PhatHNguyen <66038812+PhatHNguyen@users.noreply.github.com>
2024-09-11 09:03:47 -0600

    Update bitGenerator2.v

--- assignment 2/bitGenerator2.v
+++ assignment 2/bitGenerator2.v
@@ -1,37 +1,13 @@
 module bitGenerator2
  (
 	input [9:0] hcount,
-			[9:0] vcount,
-	input			display_pixel,
-	input	[5:0] LEDS,		
-					
+	input [9:0] vcount,
+	input	    bright,
+	input	[5:0] LEDS,						
 	output reg [7:0] red,
-				  [7:0] blue,
-				  [7:0] green
+	output reg [7:0] blue,
+	output reg [7:0] green
  );
- 
- reg L1;
- reg L2;
- reg L3;
- reg R1;
- reg R2;
- reg R3;
- reg displayWidth;
- reg displayHeight;
- 
- always@(*) begin 
-	L1 <= (400 <= hcount && 440 > hcount);
-	L2 <= (330 <= hcount && 370 > hcount);
-	L3 <= (260 <= hcount && 300 > hcount);
-	R1 <= (480 <= hcount && 520 > hcount);
-	R2 <= (550 <= hcount && 590 > hcount);
-	R3 <= (620 <= hcount && 660 > hcount);
-	displayWidth <= (220 < vcount && 260 > vcount);
-	displayHeight <= (L1 | L2 | L3| R1| R2 | R3);
- end 
- 
- 
- 
  
  
  always@(*) begin	
@@ -39,42 +15,46 @@ module bitGenerator2
 		red   <= 0;
 		blue  <= 0;
 		green <= 0;
-	end else if(~display_pixel) begin
+	end else if(~bright) begin
 		red   <= 0;
 		blue  <= 0;
 		green <= 0;
 	end
 	
-	if(displayWidth && displayHeight) begin
-		if(L1 && LEDS[3]) begin
+	if((220 < vcount) & (260 > vcount)) begin
+		if(((400 <= hcount) && (440 > hcount)) && LEDS[3]) begin
 			red <= 8'b10001001;
 			green <= 8'b11001111;
 			blue <= 8'b11110000;
-		end else if(L2 && LEDS[4]) begin
+		end else if(((330 <= hcount) && (370 > hcount)) && LEDS[4]) begin
 			red <= 8'b10001001;
 			green <= 8'b11001111;
 			blue <= 8'b11110000;	
-		end else if(L3 && LEDS[5]) begin
+		end else if(((260 <= hcount) && (300 > hcount)) && LEDS[5]) begin
 			red <= 8'b10001001;
 			green <= 8'b11001111;
 			blue <= 8'b11110000;	
-		end else if(R1 && LEDS[2]) begin 
+		end else if(((480 <= hcount) && (520 > hcount)) && LEDS[2]) begin 
 			red <= 8'b10001001;
 			green <= 8'b11001111;
 			blue <= 8'b11110000;	
-		end else if(R2 && LEDS[1]) begin 
+		end else if(((550 <= hcount) && (590 > hcount)) && LEDS[1]) begin 
 			red <= 8'b10001001;
 			green <= 8'b11001111;
 			blue <= 8'b11110000;	
-		end else if(R3 && LEDS[0]) begin 
+		end else if(((620 <= hcount) && (660 > hcount)) && LEDS[0]) begin 
 			red <= 8'b10001001;
 			green <= 8'b11001111;
 			blue <= 8'b11110000;	
-		end
+		end else begin
+			red   <= 8'b00000000;
+			blue  <= 8'b00000000;
+			green <= 8'b00000000;
+		end 
 	end else begin
-		red   <= 0;
-		blue  <= 0;
-		green <= 0;
+		red   <= 8'b00000000;
+		blue  <= 8'b00000000;
+		green <= 8'b00000000;
 	end
 end	
 endmodule
