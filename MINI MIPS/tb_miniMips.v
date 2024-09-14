46a67b90c2a73c32a095d5c1e45b4dea8ae0bd1a
PhatHNguyen <66038812+PhatHNguyen@users.noreply.github.com>
2024-09-14 16:10:49 -0600

    Update tb_miniMips.v

--- MINI MIPS/tb_miniMips.v
+++ MINI MIPS/tb_miniMips.v
@@ -1,22 +1,30 @@
+/* File Name: tb_miniMips.v
+* Author:  Phat Nguyen
+* Create Date:    09/08/2024
+* Purpose: test the mipscpu_mem with the given fibonacci code to see if it stores the 8th number into 255
+* ECE 3710 MINI MIPS
+*/
 module tb_miniMips;
 
+//instanstiate inputs and outputs 
   reg clk;
   reg rst;
   reg [7:0] switches;
   wire [7:0] LEDS;
-  
+
+//instanstiate module that has mipscpu and exmem
  miniMips UUT (.clk(clk),
-				  .rst(rst), 
-				  .switches(switches),
-				  .LEDS(LEDS)
+		.rst(rst), 
+		.switches(switches),
+		.LEDS(LEDS)
 );
 
+// clk generator 
 always 
 begin
-clk <= 1; #5; clk <= 0; #5;
+  clk <= 1; #5; clk <= 0; #5;
 end
 
-always #5 begin clk = ~clk; end
   // Generate the inputs, and check the outputs. 		
   initial begin
   switches = 8'b00000000;
