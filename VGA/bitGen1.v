7ad75bd0df771718ec2a106074376c66a8680f6e
PhatHNguyen <66038812+PhatHNguyen@users.noreply.github.com>
2024-09-14 16:36:31 -0600

    Update bitGen1.v

--- VGA/bitGen1.v
+++ VGA/bitGen1.v
@@ -1,15 +1,22 @@
+/* File Name: miniMips.v
+* Author:  Phat Nguyen
+* Create Date:    09/08/2024
+* Purpose: refresh and display one color on the entire screen 
+* ECE 3710 VGA
+*/
 module bitGen1
  (
-	input [2:0] switches,
-	input [9:0] hcount,
-	input [9:0] vcount,
-	input bright,
+	 input [2:0] switches, // allow user to decide what color is begin display
+	 input [9:0] hcount, // x position on the screen
+	 input [9:0] vcount, // y position on the screen
+	input bright,     // at the specified psoition, turn on/off the that spot
 					
-	output reg [7:0] red,
+        output reg [7:0] red, 
 	output reg [7:0] blue,
 	output reg [7:0] green
  );
- 
+
+// Option of colors that can be picked via switches
  parameter Red      = 3'b000;
  parameter Green    = 3'b001;
  parameter Blue     = 3'b010;
@@ -20,18 +27,19 @@ module bitGen1
  parameter Black    = 3'b111;
  
 always@(*) begin
-	
+	// if hcount is not in active state of screen set red,blue, and green to 0 (no color display)
 	if(hcount < 144 || hcount >= 784) begin
 		red   <= 0;
 		blue  <= 0;
 		green <= 0;
+	// user dont want it to screen to display color. set red,blue, and green to 0 (no color display)
 	end else begin 
 		if(~bright) begin
 			red   <= 0;
 			blue  <= 0;
 			green <= 0;
 		end
-		
+		// depending on the status of the switches, set the entire screen to specific color by setting the outputs red,blue, and green to specifiy value
 		case(switches)
 			Red: begin
 				red <= 8'b10000000;
@@ -68,6 +76,7 @@ always@(*) begin
 				green <= 8'b00000000;
 				blue <= 8'b00000000;
 			end
+			// This should never happen
 			default: begin
 				red <= 8'b10000000;
 				green <= 8'b00000000;
