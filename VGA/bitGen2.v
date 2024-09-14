8b54afb6c4a1066bfcbf5b2c96df1b5c830bd4e6
PhatHNguyen <66038812+PhatHNguyen@users.noreply.github.com>
2024-09-14 17:12:01 -0600

    Update bitGen2.v

--- VGA/bitGen2.v
+++ VGA/bitGen2.v
@@ -16,47 +16,58 @@ module bitGen2
  );
  
  
- always@(*) begin	
+ always@(*) begin
+	 // if hcount is not in active state of screen set red,blue, and green to 0 (no color display)
 	if(hcount < 144 || hcount >= 784) begin
 		red   <= 0;
 		blue  <= 0;
 		green <= 0;
+	// if user dont want the screen to display. set red,blue, and green to 0 (no color display)
 	end else if(~bright) begin
 		red   <= 0;
 		blue  <= 0;
 		green <= 0;
 	end
-	
+
+	 // if the vcount is with in the correct position on the grid, check for hcount and LEDS from ThunderBirdSignal 
 	if((220 < vcount) & (260 > vcount)) begin
+		// if h count within the area and the first left signal is on, set the pixel to be yellow
 		if(((400 <= hcount) && (440 > hcount)) && LEDS[3]) begin
 			red <= 8'b10001001;
 			green <= 8'b11001111;
 			blue <= 8'b11110000;
+		// if h count within the area and the second left signal is on, set the pixel to be yellow
 		end else if(((330 <= hcount) && (370 > hcount)) && LEDS[4]) begin
 			red <= 8'b10001001;
 			green <= 8'b11001111;
 			blue <= 8'b11110000;	
+		// if h count within the area and the third left signal is on, set the pixel to be yellow
 		end else if(((260 <= hcount) && (300 > hcount)) && LEDS[5]) begin
 			red <= 8'b10001001;
 			green <= 8'b11001111;
 			blue <= 8'b11110000;	
+		// if h count within the area and the first right signal is on, set the pixel to be yellow
 		end else if(((480 <= hcount) && (520 > hcount)) && LEDS[2]) begin 
 			red <= 8'b10001001;
 			green <= 8'b11001111;
 			blue <= 8'b11110000;	
+		// if h count within the area and the second right signal is on, set the pixel to be yellow
 		end else if(((550 <= hcount) && (590 > hcount)) && LEDS[1]) begin 
 			red <= 8'b10001001;
 			green <= 8'b11001111;
 			blue <= 8'b11110000;	
+		// if h count within the area and the third right signal is on, set the pixel to be yellow
 		end else if(((620 <= hcount) && (660 > hcount)) && LEDS[0]) begin 
 			red <= 8'b10001001;
 			green <= 8'b11001111;
 			blue <= 8'b11110000;	
+		// if hcount is not in the specified area or LEDS are not on then set the specific pixel to white
 		end else begin
 			red   <= 8'b00000000;
 			blue  <= 8'b00000000;
 			green <= 8'b00000000;
 		end 
+		// if vcount is not in the specified area then set the specific pixel to white
 	end else begin
 		red   <= 8'b00000000;
 		blue  <= 8'b00000000;
