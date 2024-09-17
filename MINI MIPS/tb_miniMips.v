7a75bb37bcdb169a552ea8fdfd6fd3757516e90a
PhatHNguyen <66038812+PhatHNguyen@users.noreply.github.com>
2024-09-16 22:08:53 -0600

    Update tb_miniMips.v

--- MINI MIPS/tb_miniMips.v
+++ MINI MIPS/tb_miniMips.v
@@ -1,7 +1,9 @@
 /* File Name: tb_miniMips.v
 * Author:  Phat Nguyen
 * Create Date:    09/08/2024
-* Purpose: test the mipscpu_mem with the given fibonacci code to see if it stores the 8th number into 255
+* Purpose: test the mipscpu_mem with my fibonacci code to see if it stores the 
+* first 13th numbers in address 128 - 141, afterwards see if outputs the correct fibonacci number
+* when given a specifc input switches
 * ECE 3710 MINI MIPS
 */
 module tb_miniMips;
@@ -24,23 +26,26 @@ always
 begin
   clk <= 1; #5; clk <= 0; #5;
 end
-
-  // Generate the inputs, and check the outputs. 		
+	
   initial begin
+  // reset the MIPSCPU and exmem
   switches = 8'b00000000;
   rst = 0; 
-
   #40
-  rst = 1;
-  
+  rst = 1;	
+  //generate a varitety of input switches to check for LEDS outputs 
+ #6000
+  switches = 8'b00000111; 
   #1000
-  switches = 8'b00001000;
-  #6000
-  switches = 8'b00001001;
+  switches = 8'b00000000;
   #350
+  switches = 8'b00001000;
+  #350
+  switches = 8'b00000010;
+  #500
   switches = 8'b00001010;
   end
+
 	
 endmodule
-	
 	
