87f7c0665e7c822a2013d59198e607be2c47e2cd
PhatHNguyen <66038812+PhatHNguyen@users.noreply.github.com>
2024-09-16 21:44:13 -0600

    Update fibn.asm

--- MINI MIPS/fibn.asm
+++ MINI MIPS/fibn.asm
@@ -13,13 +13,14 @@
 #    f1 = f1 + f2;
 #    f2 = f1 - f2;
 #    fibonacci_array[i] = f1;
+#     i = i + 1;
 #     n = n - 1;
 #   }
 #   return f1;
 #  }
 #
 #
-# Register usage: $3=n, $4=f1, $5=f2
+# Register usage: $3=n, $4=f1, $5=f2, $6 = i, $7 = LED output
 # store the Fibonacci numbers in a specific address
 
 addi $3, $0, 13		# initialize n = 13
@@ -34,8 +35,8 @@ loop:	beq $3, $0, checkForInputs   # Done with loop if n = 0
 	addi $3, $3, -1		# n = n - 1
 	j loop                  # repeat until done
 checkForInput:                
-	lb $6, 255($0)         # Load the array item indicated by the switch inputs to output, LED.
-	lb $7, 128($6)
-	sb $7 255($0)
-	j checkForInput
+	lb $6, 255($0)         # Save the desired address into 
+	lb $7, 128($6)         # load the desired value into register 7
+	sb $7 255($0)          # save the value from register 7 into address 255 to be display 
+	j checkForInput       # jump back to checkForInput
 exit:
