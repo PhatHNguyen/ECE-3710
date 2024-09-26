a4cce9898d6712b58e1d65f631dd6ecd857d0de7
PhatHNguyen <66038812+PhatHNguyen@users.noreply.github.com>
2024-09-26 07:38:08 -0600

    Update bitGen1.v

--- VGA/bitGen1.v
+++ VGA/bitGen1.v
@@ -66,6 +66,11 @@ always@(*) begin
 				green <= 8'b11111111;
 				blue <= 8'b00000000;
 			end
+			Pink: begin
+				red   <= 8'b11111111;
+				green <= 8'b11100000;
+				blue  <= 8'b11100000;
+			end
 			White: begin
 				red <= 8'b11111111;
 				green <= 8'b11111111;
